dbe6d1c20d3fa00d161fabe513f44fc8ce0631fa
maryamagaiby <maryamagaiby@gmail.com>
2026-05-07 11:34:44 +0300

    push again

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module org.example.open_scholars {
     requires java.sql;
     requires java.desktop;
     //requires jdk.jconsole;
+    //push
     opens com.university.model to javafx.base;
     exports com.university.ui;
     opens com.university.ui to javafx.fxml;
